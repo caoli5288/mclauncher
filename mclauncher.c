9250ec97c6f7bd706c9d08ce8022d588bae6f7b1
caoli5288 <caoli5288@gmail.com>
2013-05-18 16:31:36 +0800

    Update mclauncher.c

--- mclauncher.c
+++ mclauncher.c
@@ -1,7 +1,24 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
+
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <direct.h>
 #include <unistd.h>
 
 void initgamelinux(char *player)
@@ -53,8 +70,8 @@ void startgame(GtkWidget *button,gpointer bufferplayer)
 	chdir("./bin");
 	g_print("%s\n",getcwd(NULL,0));
 	
-	initgamelinux(player);
-	//initgamems(player);
+	//initgamelinux(player);
+	initgamems(player);
 	}
 
 void findgamelinux()
@@ -112,8 +129,8 @@ void initmainwin()
 	hbox1 = gtk_hbox_new(FALSE,1);
 	gtk_box_pack_start(GTK_BOX(vbox2),hbox1,FALSE,FALSE,0);	
 	
-	findgamelinux();
-	//findgamems();
+	//findgamelinux();
+	findgamems();
 	
 	pconf = fopen("player.conf","a+");
 	fgets(player,80,pconf);
