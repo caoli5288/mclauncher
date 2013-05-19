20ca3652a2f0d63dbf8b0603d6ba980ab61e02dd
caoli5288@gmail.com <yiwan@localhost.localdomain>
2013-05-20 01:32:03 +0800

    修改：      mclauncher 	修改：      mclauncher.c 	修改：      mclauncher.png

--- mclauncher.c
+++ mclauncher.c
@@ -21,7 +21,24 @@
 #include <string.h>
 #include <unistd.h>
 
-void init_game_linux(char *player)
+void player_load(gchar *player)
+{
+	FILE *pconf;
+	pconf = fopen("player.conf","a+");
+	fgets(player,80,pconf);
+	fclose(pconf);
+}
+
+void player_save(gchar *player)
+{
+	FILE *pconf;
+	pconf = fopen("player.conf","w+");	
+	fputs(player,pconf);
+	fclose(pconf);
+	g_print("save player.conf complete\n");
+	}
+
+void init_game_linux(gchar *player)
 {
 	char com[200] = "java -Xms512m -Xmx1g -cp ";
 	strcat(com,"jinput.jar:lwjgl.jar:lwjgl_util.jar:minecraft.jar ");
@@ -29,114 +46,55 @@ void init_game_linux(char *player)
 	strcat(com,"net.minecraft.client.Minecraft ");
 	strcat(com,player);
 	
-	g_print("\n%s",com);
+	g_print("%s\n",com);
 	
-	execl ("/bin/bash", "bash", "-c", com, NULL);
+	execlp ("bash", "bash", "-c", com, NULL);
 	}
 	
 void init_game_ms(char *player)
 {
-	execlp("javaw","javaw","-cp","/jinput.jar;lwjgl.jar;lwjgl_util.jar;minecraft.jar","-Djava.library.path=\"./natives\"","net.minecraft.client.Minecraft",player,NULL);
-	}
-	
-void set_conf_win()
-{
-	
-	}
-	
-void point_down_pop()
-{
 
 	}
 	
-void gsign_set_conf(GtkWidget *button,gpointer userdata)
-{
-	set_conf_win();
-	}
 	
-void gsign_chose_game_path(GtkWidget *button,gpointer userdata)
+void gsign_open_home(GtkWidget *button,gpointer userdata)
 {
-	
+	execlp("firefox","firefox","http://mc.yiraft.tk",NULL);            //用于linux
+	execlp("iexplore","iexplore","http://mc.yiraft.tk",NULL);        //用于windows
 	}
 
-void gsign_start_game(GtkWidget *button,gpointer bufferplayer)
-{
-	FILE *pconf;
-	char player[80] = "";
-	
-	pconf = fopen("player.conf","w+");
-	strcat(player,gtk_entry_buffer_get_text(bufferplayer));
-	
-	fputs(player,pconf);
-	fclose(pconf);
-	
-	g_print("%s\n",player);
+
+void gsign_start_game(GtkWidget *button,gpointer player_buffer)
+{	
+	gchar player[80] = "";
+	strcat(player,gtk_entry_buffer_get_text(player_buffer));
+	player_save(player);
 	
 	chdir("./bin");
 	g_print("%s\n",getcwd(NULL,0));
 	
-	init_game_linux(player);                           //用于linux
-	//init_game_ms(player);                                 //用于windows
+	//init_game_linux(player);                           //用于linux
+	init_game_ms(player);                                 //用于windows
 	}
 
-void chose_game_path_win()
+void win_erro(GtkWidget *window)
 {
-	
+	GtkWidget *dialog;	
+	dialog = gtk_message_dialog_new(GTK_WINDOW(window), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,
+	"当前用户家目录下没有.minecraft文件夹\n\n如果您还没有下载游戏,可以到这里下载\n\nhttp://date.yiraft.tk/");
+	gtk_window_set_title(GTK_WINDOW(dialog), "找不到游戏");
+	gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(dialog);
 	}
 
-void no_game_pop()
-{
-	GtkWidget *window1;
-	GtkWidget *vbox1;
-	GtkWidget *hbox1;
-	GtkWidget *label1;
-	GtkWidget *button1,*button2;
-	gchar *words;
-	
-	window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window1),"Yiwan's craft Launcher");
-	gtk_window_set_default_size(GTK_WINDOW(window1),400,150);
-	gtk_widget_set_size_request (window1, 400, 150);
-	//gtk_window_set_resizable(GTK_WINDOW(window1),FALSE);
-	gtk_window_set_position(GTK_WINDOW(window1),GTK_WIN_POS_CENTER);
-	gtk_container_set_border_width(GTK_CONTAINER(window1),20);
-	g_signal_connect(G_OBJECT(window1),"delete_event",G_CALLBACK(gtk_main_quit),NULL);
-	
-	vbox1 = gtk_vbox_new(FALSE,2);
-	gtk_container_add(GTK_CONTAINER(window1),vbox1);
-	
-	words = "<i><big>在用户目录下找不到游戏文件夹!\n请选择一个包含有.minecraft文件夹的路径!\n单击退出程序以结束。</big></i>";
-	label1 = gtk_label_new(NULL);
-	gtk_label_set_markup(GTK_LABEL(label1),words);
-	gtk_box_pack_start(GTK_BOX(vbox1),label1,FALSE,FALSE,0);
-	
-	hbox1 = gtk_hbox_new(TRUE,2);
-	
-	gtk_box_pack_end(GTK_BOX(vbox1),hbox1,FALSE,TRUE,0);
-	
-	button1 = gtk_button_new_with_label("设置路径");
-	button2 = gtk_button_new_with_label("退出程序");
-	
-	//g_signal_connect(G_OBJECT(button1),"clicked",G_CALLBACK(gsign_chose_game_path),NULL);
-	g_signal_connect(G_OBJECT(button2),"clicked",G_CALLBACK(gtk_main_quit),NULL);
-	
-	gtk_box_pack_start(GTK_BOX(hbox1),button1,FALSE,FALSE,0);
-	gtk_box_pack_start(GTK_BOX(hbox1),button2,FALSE,FALSE,0);
-	
-	gtk_widget_show_all(window1);
-
-	//gtk_main();
-	gtk_main_quit();
-	}
-
-int find_game_linux()
+int find_game_linux(GtkWidget *window)
 {
 	char path[200]="";
 	strcat(path,getenv("HOME"));
 	strcat(path,"/.minecraft");
 	if(chdir(path) != 0 )
 	{
-		no_game_pop();
+		win_erro(window);
 		return -1;
 			}
 	else
@@ -145,10 +103,10 @@ int find_game_linux()
 	return 0;
 	}
 
-int find_game_ms()
+int find_game_ms(GtkWidget *window)
 {
 	if(chdir("./.minecraft") != 0 )
-		{	no_game_pop();
+		{	win_erro(window);
 			return -1;
 		}
 	else
@@ -157,78 +115,77 @@ int find_game_ms()
 	return 0;
 	}
 
-void main_win()
+void win_main(GtkWidget *window)
 {
-	char player[80] = "";
-	FILE *pconf;
-	GtkWidget *window1;
 	GtkWidget *vbox1,*vbox2,*hbox1,*hbox2;
-	GtkWidget *image1;
 	GtkWidget *label1;
+	GtkWidget *image;
 	GtkWidget *entryplayer;
 	GtkWidget *button1,*button2;
-	GtkEntryBuffer *bufferplayer;
+	GtkEntryBuffer *player_buffer;
+	gchar player[80] = "";
 	
-	window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window1),"Yiwan's craft Launcher");
-	gtk_window_set_default_size(GTK_WINDOW(window1),320,400);
-	gtk_widget_set_size_request (window1, 320, 400);
-	gtk_window_set_resizable(GTK_WINDOW(window1),FALSE);
-	gtk_window_set_position(GTK_WINDOW(window1),GTK_WIN_POS_CENTER);
-	gtk_container_set_border_width(GTK_CONTAINER(window1),10);
-	g_signal_connect(G_OBJECT(window1),"delete_event",G_CALLBACK(gtk_main_quit),NULL);
+	player_load(player);
+	
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window),"Yiwan's craft Launcher");
+	gtk_window_set_default_size(GTK_WINDOW(window),320,0);
+	gtk_window_set_resizable(GTK_WINDOW(window),FALSE);
+	gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER);
+	gtk_container_set_border_width(GTK_CONTAINER(window),10);
+	g_signal_connect(G_OBJECT(window),"delete_event",G_CALLBACK(gtk_main_quit),NULL);
 	
 	vbox1 = gtk_vbox_new(FALSE,2);
-	gtk_container_add(GTK_CONTAINER(window1),vbox1);
+	gtk_container_add(GTK_CONTAINER(window),vbox1);
 	
-	image1 = gtk_image_new_from_file("mclauncher.png");
-	gtk_box_pack_start(GTK_BOX(vbox1),image1,FALSE,FALSE,0);
+	image = gtk_image_new_from_file("mclauncher.png");
+	gtk_box_pack_start(GTK_BOX(vbox1),image,FALSE,FALSE,0);
 	
 	vbox2 = gtk_vbox_new(FALSE,3);
 	gtk_box_pack_start(GTK_BOX(vbox1),vbox2,TRUE,TRUE,0);
 	
-	label1 = gtk_label_new("输入你的用户名");
+	label1 = gtk_label_new("==============输入你的玩家名==============");
 	gtk_box_pack_start(GTK_BOX(vbox2),label1,FALSE,FALSE,10);
 	
 	hbox1 = gtk_hbox_new(FALSE,1);
 	gtk_box_pack_start(GTK_BOX(vbox2),hbox1,FALSE,FALSE,0);	
-	
-	if(find_game_linux() != 0 )                          //用于linux
-	//if(find_game_ms() != 0 )                                //用于windows
-	return;
-	
-	pconf = fopen("player.conf","a+");
-	fgets(player,80,pconf);
-	fclose(pconf);
-	
+		
 	g_print("%s\n",player);
 	
-	bufferplayer = gtk_entry_buffer_new(player,-1);
+	player_buffer = gtk_entry_buffer_new(player,-1);
 	
-	entryplayer = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(bufferplayer));
-	gtk_box_pack_start(GTK_BOX(hbox1),entryplayer,TRUE,TRUE,60);
+	entryplayer = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(player_buffer));
+	gtk_entry_set_alignment (GTK_ENTRY (entryplayer), 0.5);
+	gtk_box_pack_start(GTK_BOX(hbox1),entryplayer,TRUE,TRUE,0);
 		
 	hbox2 = gtk_hbox_new(TRUE,2);
 	gtk_box_pack_end(GTK_BOX(vbox2),hbox2,FALSE,FALSE,10);
 	
-	button1 = gtk_button_new_with_label("设置选项");
+	button1 = gtk_button_new_with_label("打开主页");
 	button2 = gtk_button_new_with_label("启动游戏");
 	
-	g_signal_connect(G_OBJECT(button1),"clicked",G_CALLBACK(gsign_set_conf),NULL);
-	g_signal_connect(G_OBJECT(button2),"clicked",G_CALLBACK(gsign_start_game),bufferplayer);
+	g_signal_connect(G_OBJECT(button1),"clicked",G_CALLBACK(gsign_open_home),NULL);
+	g_signal_connect(G_OBJECT(button2),"clicked",G_CALLBACK(gsign_start_game),player_buffer);
 	
-	gtk_box_pack_start(GTK_BOX(hbox2),button1,FALSE,FALSE,0);
-	gtk_box_pack_start(GTK_BOX(hbox2),button2,FALSE,FALSE,0);
+	gtk_box_pack_start(GTK_BOX(hbox2),button1,FALSE,TRUE,0);
+	gtk_box_pack_start(GTK_BOX(hbox2),button2,FALSE,TRUE,0);
 	
-	gtk_widget_show_all(window1);	
+	gtk_widget_show_all(window);	
 	}
 	
 int main(int argc, char **argv)
-{	
+{		
+	GtkWidget *window;
+	
 	gtk_init(&argc,&argv);
-	main_win();
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	
+	//if(find_game_linux(window) != 0 )                          //用于linux
+	if(find_game_ms(window) != 0 )                                //用于windows
+	return 0;
+
+	win_main(window);
 	gtk_main();
 	
 	return 0;
-}
-
+	}
